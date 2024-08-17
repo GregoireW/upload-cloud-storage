4ac2eeba706588c37334cedb60c564701c0c204f
GregoireW <24318548+GregoireW@users.noreply.github.com>
2024-08-17 12:02:24 +0200

    debug

--- src/client.ts
+++ src/client.ts
@@ -166,13 +166,6 @@ export class Client {
       projectId: opts?.projectID,
       universeDomain: opts?.universe,
     });
-    /*
-    auth.getClient().then((client) => {
-      console.log('The created client is: ', client);
-      client.on('tokens', (token) => console.dir({ expiry_date: token.expiry_date }));
-      client.getAccessToken();
-    });
-    */
 
     const options: StorageOptions = {
       authClient: auth,
